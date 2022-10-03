1f39bdf9296f9a062bd347d6d87e9d635f6efb73
PunchlY <punchly@foxmail.com>
2022-10-04 02:18:00 +0800

    -

--- solve/index.js
+++ solve/index.js
@@ -1,6 +1,3 @@
-const Rubik = require('../index');
-
-Rubik.basisSolutionList = require('./solve.json');
 const Re = [
     'R', 'R2', 'R\'',
     'U', 'U2', 'U\'',
@@ -27,31 +24,35 @@ const RT = [
     M[3], M[9], M[6],
     M[13], M[15], M[4],
 ];
-Rubik.prototype.solve = function (t = NaN) {
-    const raw = (() => {
-        const call = {};
-        for (const { r, c: [c, cT], image, inverse } of this.similar(0)) {
-            const e = this.constructor.basisSolutionList[r.position];
-            if (e === undefined) continue;
-            let E = [].concat(e.split('')).map((v) => ~~v);
-            image && (E = E.map((v) => (~~(v / 3) || 3) * 3 + (2 - v % 3)));
-            !inverse && (E = E.reverse().map((v) => ~~(v / 3) * 3 + (2 - v % 3)));
-            E = E.map(function (v) { return this[~~(v / 3)] * 3 + v % 3 }, M[((c) => image ? c.image()[0] : c[0])(inverse ? c.inverse() : cT)]);
-            call.raw = E;
-            break;
-        }
-        return call.raw;
-    })();
-    if (!(t === null || isNaN(t))) [...function* (t, i) {
-        t &= (1 << i) - 1;
-        while (i--) yield t & 1, t >>>= 1;
-    }(t, raw.length)].forEach(function (b, i) {
-        let v = raw[i];
-        v = this[~~(v / 3)] * 3 + v % 3;
-        raw[i] = b * 9 + v % 9;
-        if (b ^ ~~(v / 9)) RT[v].map((_, i, a) => this[i] = a[this[i]]);
-    }, M[0]);
-    return raw.map((v) => Re[v]).join('');
-};
+
+class Rubik extends require('../index') {
+    static basisSolutionList = require('./solve.json')
+    solve(t = NaN) {
+        const raw = (() => {
+            const call = {};
+            for (const { r, c: [c, cT], image, inverse } of this.similar(0)) {
+                const e = this.constructor.basisSolutionList[r.position];
+                if (e === undefined) continue;
+                let E = [].concat(e.split('')).map((v) => ~~v);
+                image && (E = E.map((v) => (~~(v / 3) || 3) * 3 + (2 - v % 3)));
+                !inverse && (E = E.reverse().map((v) => ~~(v / 3) * 3 + (2 - v % 3)));
+                E = E.map(function (v) { return this[~~(v / 3)] * 3 + v % 3 }, M[((c) => image ? c.image()[0] : c[0])(inverse ? c.inverse() : cT)]);
+                call.raw = E;
+                break;
+            }
+            return call.raw;
+        })();
+        if (!(t === null || isNaN(t))) [...function* (t, i) {
+            t &= (1 << i) - 1;
+            while (i--) yield t & 1, t >>>= 1;
+        }(t, raw.length)].forEach(function (b, i) {
+            let v = raw[i];
+            v = this[~~(v / 3)] * 3 + v % 3;
+            raw[i] = b * 9 + v % 9;
+            if (b ^ ~~(v / 9)) RT[v].map((_, i, a) => this[i] = a[this[i]]);
+        }, M[0]);
+        return raw.map((v) => Re[v]).join('');
+    }
+}
 
 module.exports = Rubik;

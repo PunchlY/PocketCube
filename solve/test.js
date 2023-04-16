9981b1ba1af6fe86ed860dcf050105aad81fa457
PunchlY <punchly@foxmail.com>
2023-04-16 22:52:33 +0800

    modified:   solve/test.js

--- solve/test.js
+++ solve/test.js
@@ -1,4 +1,6 @@
 import { Rubik } from '../dist/PocketCube.solve.min.mjs';
 
-for (const solv of Rubik.solveAll(process.argv[2], true))
-    console.log(new Rubik().do(process.argv[2]).do(solv).isReinstated());
+for (const solv of Rubik.solveAll(process.argv[2], true)) {
+    // console.log(new Rubik().do(process.argv[2]).do(solv).isReinstated());
+    console.log(solv);
+}
